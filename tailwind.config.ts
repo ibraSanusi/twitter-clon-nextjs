0af422deb50b9458ee72af6c82c7d8e7983b2c78
ibraSanusi <isanu227@gmail.com>
2024-03-13 02:37:52 +0100

    Added template columns customization.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      gridTemplateColumns: {
+        layout: "320px minmax(500px, 1fr) 320px",
+      },
     },
   },
   plugins: [],
