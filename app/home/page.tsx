b05f8faa839817ff5987441e5314e7b6a641ee36
ibraSanusi <isanu227@gmail.com>
2024-03-13 02:42:23 +0100

    Modified sidebar component and refactored tweetpost component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,19 @@
+import TweetCards from '../ui/TweetCards'
+import TweetPost from '../ui/TweetPost'
+
 export default function Page() {
-  return <strong>Funciona</strong>
+  return (
+    <>
+      <div className="flex flex-row justify-between mb-6">
+        <h1 className="text-2xl font-bold">Feeds</h1>
+        <div className="flex flex-row gap-4 font-bold">
+          <span className="text-black">Friends</span>
+          <span className="text-gray-300">Popular</span>
+        </div>
+      </div>
+
+      <TweetPost />
+      <TweetCards />
+    </>
+  )
 }
--- app/ui/SideBar.tsx
+++ app/ui/SideBar.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import NavLinks from './NavLinks'
 
-export default function SideBar() {
+interface props {
+  className: string
+}
+
+export default function SideBar({ className }: props) {
   return (
-    <section className="px-6 py-4 h-[100dvh] w-80">
-      <header className="w-full my-8 flex flex-col gap-1 justify-center items-center">
+    <section className={className}>
+      <header className="w-full mb-8 flex flex-col gap-1 justify-center items-center">
         <Image
           src="/delba_oliveira.webp"
           alt="Imagen de perfil de delba TODO"
