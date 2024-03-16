22ddfeda9b975697b062e4dc9cf314e644550257
ibraSanusi <isanu227@gmail.com>
2024-03-16 14:45:47 +0100

    Created post and register actions. Login still on process

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,17 +1,56 @@
 'use server'
 
-// const { sql } = require('@vercel/postgres')
-import { sql } from '@vercel/postgres'
+import { db } from '../services/db'
 
+// Hacer un post
 export async function postTweet(formData: FormData) {
   const content = formData.get('content')?.toString()
 
-  const client = await sql.connect()
-
-  const insertPost = client.sql`
-    INSERT INTO "Post" (content)
-    values (${content})
-  `
+  if (content) {
+    await db.post.create({
+      data: {
+        content,
+      },
+    })
+  }
 
   console.log(content)
+}
+
+// Iniciar sesion
+export async function login(formData: FormData) {
+  const content = formData.get('content')?.toString()
+
+  if (content) {
+    await db.post.create({
+      data: {
+        content,
+      },
+    })
+  }
+
+  console.log(content)
+}
+
+// Registrarse
+export async function register(formData: FormData) {
+  const fullname = formData.get('fullname')?.toString()
+  const username = formData.get('username')?.toString()
+  const email = formData.get('email')?.toString()
+  const password = formData.get('password')?.toString()
+
+  const data = { fullname, username, email, password }
+
+  if (fullname && username && email && password) {
+    await db.user.create({
+      data: {
+        email,
+        fullname,
+        username,
+        password,
+      },
+    })
+  }
+
+  console.log(email, fullname, username, password)
 }
