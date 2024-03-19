c5e4904b1ffe2305b5b6c3fd5cad44e64cb5925f
ibraSanusi <isanu227@gmail.com>
2024-03-19 20:37:03 +0100

    Obtimized link and changed ui

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,21 +2,22 @@ import { register } from '@/lib/actions'
 import { EyeIcon } from '@heroicons/react/16/solid'
 import { EyeSlashIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Page() {
   return (
-    <section className="grid grid-cols-2 justify-center w-full h-screen py-8">
+    <section className="grid grid-cols-2 justify-center w-full h-dvh py-8">
       {/* Left section */}
       <article className="flex items-end justify-end">
         <div className="w-[629px] h-[664px]">
           <h1 className="text-4xl mb-6 font-bold">Sign Up to</h1>
           <h2 className="text-3xl">Sintoniza tu onda</h2>
           <div className="flex flex-row">
-            <p className="pt-16">
+            <p className="pt-16 text-pretty h-min">
               Si ya tienes una cuenta puedes iniciar sesión{' '}
-              <a className="text-auth" href="">
+              <Link className="text-auth" href={'/auth/login'}>
                 aquí!
-              </a>
+              </Link>
             </p>
 
             <Image
@@ -24,7 +25,7 @@ export default function Page() {
               alt="Imagen de persona chateando con un movil."
               width={313}
               height={556}
-              className="w-[313px] h-[556]"
+              className="w-[313px] h-[556px]"
             />
           </div>
         </div>
