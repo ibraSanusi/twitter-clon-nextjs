957cfdd1986e8fc7ded88a1382dc032ac5fa5169
ibraSanusi <isanu227@gmail.com>
2024-04-22 01:33:57 +0200

    Protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,12 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/home', '/dashboard'],
+  matcher: [
+    '/home',
+    '/dashboard',
+    '/explore',
+    '/message',
+    '/profile',
+    '/settings',
+  ],
 }
