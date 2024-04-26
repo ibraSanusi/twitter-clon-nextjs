efbe6e61d874cc3f58b39c145079bdd8196b593e
ibraSanusi <isanu227@gmail.com>
2024-04-27 01:29:04 +0200

    updated data base schema

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,11 +1,16 @@
+'use client'
+
 import SideBar from '@/app/ui/SideBar'
+import { SessionProvider } from 'next-auth/react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="grid grid-cols-layout max-w-[1300px] m-auto">
-      <SideBar className="px-6 py-4 h-screen w-80" />
-      <section className="flex flex-col px-6 py-4">{children}</section>
-      {/* <SideBar className="px-6 py-4 h-screen w-80" /> */}
-    </main>
+    <SessionProvider>
+      <main className="grid grid-cols-layout max-w-[1300px] m-auto">
+        <SideBar className="px-6 py-4 h-screen w-80" />
+        <section className="flex flex-col px-6 py-4">{children}</section>
+        {/* <SideBar className="px-6 py-4 h-screen w-80" /> */}
+      </main>
+    </SessionProvider>
   )
 }
