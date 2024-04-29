ec35cf16e2ba723adefc62fdb32252e767fc1870
ibraSanusi <isanu227@gmail.com>
2024-04-29 22:12:20 +0200

    able to post

--- app/api/post/tweet/route.ts
+++ app/api/post/tweet/route.ts
@@ -1,42 +1,54 @@
 import db from '@/services/db'
 import { NextRequest } from 'next/server'
 
-export async function POST(request: NextRequest) {
-  const body: { email: string; content: string } = await request.json()
-  console.log('BODY: ', body)
+interface TweetInterface {
+  id: string
+  content: string
+  createdAt: Date
+  updatedAt: Date
+  userId: string
+}
 
-  // 1. Sacar el id del usuario
-  const userEmail = body?.email ?? body.email
-  if (!userEmail) {
-    return
+export async function POST(request: NextRequest): Promise<Response> {
+  try {
+    const body: { email: string; content: string } = await request.json()
+
+    const userEmail = body?.email ?? body.email
+    if (!userEmail) {
+      return new Response('El correo electrónico del usuario es requerido', {
+        status: 400,
+      })
+    }
+
+    const user = await db.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    })
+
+    if (!user) {
+      return new Response('Usuario no encontrado', { status: 404 })
+    }
+
+    const userId = user.id
+
+    const content = body?.content
+    if (!content) {
+      return new Response('El contenido del tweet es requerido', {
+        status: 400,
+      })
+    }
+
+    const newTweet = await db.post.create({
+      data: {
+        userId,
+        content,
+      },
+    })
+
+    return new Response(JSON.stringify(newTweet), { status: 201 })
+  } catch (error) {
+    console.error('Error al procesar la solicitud:', error)
+    return new Response('Error interno del servidor', { status: 500 })
   }
-
-  const user = await db.user.findUnique({
-    where: {
-      email: userEmail,
-    },
-  })
-
-  if (!user) {
-    return
-  }
-
-  const userId = user.id
-
-  // 2. Poster el tweet con el id del usuario
-  const content = body?.content
-  if (!content) {
-    return
-  }
-  const tweetPosted = db.post.create({
-    data: {
-      userId,
-      content,
-    },
-  })
-
-  // 3. Devolver respuesta de la subida
-  return new Response(
-    'Subido el tweet ' + (await tweetPosted).userId + ' correctamente',
-  )
 }
