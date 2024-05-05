f3db4496fdc6aead66af75b9468d08c759ebb940
ibraSanusi <isanu227@gmail.com>
2024-05-05 20:30:15 +0200

    password visibility feature added

--- app/ui/RegisterForm.tsx
+++ app/ui/RegisterForm.tsx
@@ -1,41 +1,53 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { register } from '@/lib/actions/auth/register'
 import { EyeSlashIcon } from '@heroicons/react/20/solid'
+import { EyeIcon } from '@heroicons/react/16/solid'
 
 export default function RegisterForm() {
+  const [passwordVisibility, setPasswordVisibility] = useState<boolean>(false)
+  const handleClick = () => {
+    setPasswordVisibility(!passwordVisibility)
+  }
+
   return (
-    <form className="flex flex-col gap-4 w-full h-full" action={register}>
+    <form className="flex h-full w-full flex-col gap-4" action={register}>
       <input
-        className="bg-input p-4 rounded-md placeholder:text-placeholder focus:outline-none"
+        className="rounded-md bg-input p-4 placeholder:text-placeholder focus:outline-none"
         type="text"
         placeholder="Escribe tu email"
         name="email"
         required
       />
       <input
-        className="bg-input p-4 rounded-md placeholder:text-placeholder focus:outline-none"
+        className="rounded-md bg-input p-4 placeholder:text-placeholder focus:outline-none"
         type="text"
         placeholder="Escribe tu username"
         name="username"
         required
       />
       <input
-        className="bg-input p-4 rounded-md placeholder:text-placeholder focus:outline-none"
+        className="rounded-md bg-input p-4 placeholder:text-placeholder focus:outline-none"
         type="text"
         placeholder="Escribe tu nombre completo"
         name="fullname"
         required
       />
-      <div className="flex flex-row justify-between items-center bg-input p-4 rounded-md">
+      <div className="flex flex-row items-center justify-between rounded-md bg-input p-4">
         <input
           className="bg-transparent placeholder:text-placeholder focus:outline-none"
-          type="password"
+          type={passwordVisibility ? 'text' : 'password'}
           placeholder="Escribe tu contraseña"
           name="password"
           required
         />
-        <button className="size-5 text-placeholder">
-          <EyeSlashIcon />
+        <button
+          type="button"
+          onClick={handleClick}
+          className="size-5 text-placeholder"
+        >
+          {passwordVisibility ? <EyeIcon /> : <EyeSlashIcon />}
         </button>
       </div>
       {/* <div className="flex flex-row justify-between items-center bg-input p-4 rounded-md">
@@ -50,7 +62,7 @@ export default function RegisterForm() {
                 <EyeSlashIcon />
               </button>
             </div> */}
-      <button className="bg-button p-4 rounded-md text-white" type="submit">
+      <button className="rounded-md bg-button p-4 text-white" type="submit">
         Registrar
       </button>
     </form>
