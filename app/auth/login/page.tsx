cee2a104f03e9a986683d2bb0a6e03108a7693bf
ibraSanusi <isanu227@gmail.com>
2024-05-05 19:55:59 +0200

    moved to ui

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 
-import LoginForm from '@/app/components/forms/LoginForm'
+import LoginForm from '@/app/ui/LoginForm'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Page() {
   return (
-    <section className="grid grid-cols-2 justify-center w-full h-screen py-8">
+    <section className="grid h-screen w-full grid-cols-2 justify-center py-8">
       {/* Left section */}
       <article className="flex items-end justify-end">
-        <div className="w-[629px] h-[664px]">
-          <h1 className="text-4xl mb-6 font-bold">Sign In to</h1>
+        <div className="h-[664px] w-[629px]">
+          <h1 className="mb-6 text-4xl font-bold">Sign In to</h1>
           <h2 className="text-3xl">Sintoniza tu onda</h2>
           <div className="flex flex-row">
-            <p className="pt-16 text-pretty">
+            <p className="text-pretty pt-16">
               Si no tienes una cuenta puedes registrarte{' '}
               <Link className="text-auth" href="/auth/register">
                 aquí!
@@ -25,15 +25,15 @@ export default function Page() {
               alt="Imagen de persona chateando con un movil."
               width={313}
               height={556}
-              className="w-[313px] h-[556px]"
+              className="h-[556px] w-[313px]"
             />
           </div>
         </div>
       </article>
 
       {/* Right section */}
-      <article className="flex justify-center items-center w-full h-full">
-        <div className="flex flex-col gap-6 w-[369px] h-[700px] mx-auto">
+      <article className="flex h-full w-full items-center justify-center">
+        <div className="mx-auto flex h-[700px] w-[369px] flex-col gap-6">
           <h2 className="w-full text-2xl">Sing In</h2>
           <LoginForm />
         </div>
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,18 +1,18 @@
-import RegisterForm from '@/app/components/forms/RegisterForm'
+import RegisterForm from '@/app/ui/RegisterForm'
 
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default async function Page() {
   return (
-    <section className="grid grid-cols-2 justify-center w-full h-dvh py-8">
+    <section className="grid h-dvh w-full grid-cols-2 justify-center py-8">
       {/* Left section */}
       <article className="flex items-end justify-end">
-        <div className="w-[629px] h-[664px]">
-          <h1 className="text-4xl mb-6 font-bold">Sign Up to</h1>
+        <div className="h-[664px] w-[629px]">
+          <h1 className="mb-6 text-4xl font-bold">Sign Up to</h1>
           <h2 className="text-3xl">Sintoniza tu onda</h2>
           <div className="flex flex-row">
-            <p className="pt-16 text-pretty h-min">
+            <p className="h-min text-pretty pt-16">
               Si ya tienes una cuenta puedes iniciar sesión{' '}
               <Link className="text-auth" href={'/auth/login'}>
                 aquí!
@@ -24,15 +24,15 @@ export default async function Page() {
               alt="Imagen de persona chateando con un movil."
               width={313}
               height={556}
-              className="w-[313px] h-[556px]"
+              className="h-[556px] w-[313px]"
             />
           </div>
         </div>
       </article>
 
       {/* Right section */}
-      <article className="flex justify-center items-center w-full h-full">
-        <div className="flex flex-col gap-6 w-[369px] h-[700px] mx-auto">
+      <article className="flex h-full w-full items-center justify-center">
+        <div className="mx-auto flex h-[700px] w-[369px] flex-col gap-6">
           <h2 className="w-full text-2xl">Sing Up</h2>
           <RegisterForm />
         </div>
